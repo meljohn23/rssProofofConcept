0f263e02e1bfe556cbf6202c27eba4a263a1fd3a
Doug Hughes <doug@theironyard.com>
2016-10-06 14:27:28 -0400

    doug's syndication, sorting, and getting images

--- src/main/java/FeedAggregator.java
+++ src/main/java/FeedAggregator.java
@@ -1,13 +1,20 @@
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.feed.synd.SyndFeedImpl;
-import com.sun.syndication.io.SyndFeedInput;
-import com.sun.syndication.io.SyndFeedOutput;
-import com.sun.syndication.io.XmlReader;
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
-import java.io.PrintWriter;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndEntryImpl;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+import org.jdom2.Element;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by melaniejohnson on 9/29/16.
@@ -15,47 +22,75 @@ import java.util.ArrayList;
 public class FeedAggregator {
 
     public static void main(String[] args) {
-        boolean ok = false;
-        if (args.length>=2) try {
-            String outputType = args[0];
 
-            SyndFeed feed = new SyndFeedImpl();
-            feed.setFeedType(outputType);
+        try {
 
-            feed.setTitle("NYTimes Home Page");
-            feed.setDescription("NY Times National News Feed");
-            feed.setAuthor("New York Times");
-            feed.setLink("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml");
+            List<SyndEntry> entries = new ArrayList();
 
-            List entries = (List) new ArrayList();
-            feed.setEntries((java.util.List) entries);
+            SyndFeedInput input = new SyndFeedInput();
 
-            for (int i = 1; i < args.length; i++) {
-                URL inputUrl = new URL(args[i]);
+            SyndFeed inFeed = input.build(new XmlReader(new URL("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")));
+            entries.addAll(inFeed.getEntries());
 
-                SyndFeedInput input = new SyndFeedInput();
-                SyndFeed inFeed = input.build(new XmlReader(new URL("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")));
+            //inFeed = input.build(new XmlReader(new URL("http://feeds.washingtonpost.com/rss/world")));
+            //entries.addAll(inFeed.getEntries());
 
-                ((java.util.List) entries).addAll(inFeed.getEntries());
+            inFeed = input.build(new XmlReader(new URL("http://feeds.reuters.com/Reuters/domesticNews")));
+            entries.addAll(inFeed.getEntries());
 
+            inFeed = input.build(new XmlReader(new URL("http://hosted2.ap.org/atom/APDEFAULT/3d281c11a96b4ad082fe88aa0db04305")));
+            entries.addAll(inFeed.getEntries());
+
+            entries.sort((o1, o2) -> {
+                if(o1.getPublishedDate() == null || o2.getPublishedDate() == null){
+                    return -1;
+                }
+
+                return o2.getPublishedDate().compareTo(o1.getPublishedDate());
+            });
+
+
+            for(SyndEntry entry : entries){
+                printEntry(entry);
             }
 
-            SyndFeedOutput output = new SyndFeedOutput();
-            output.output(feed, new PrintWriter(System.out));
 
-            ok = true;
-        } catch (Exception ex) {
-            System.out.println("ERROR: " + ex.getMessage());
+        } catch (FeedException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        if (!ok) {
-            System.out.println();
-            System.out.println("FeedAggregator aggregates different feeds into a single one.");
-            System.out.println("The first parameter must be the feed type for the aggregated feed.");
-            System.out.println(" [valid values are: rss_0.9, rss_0.91, rss_0.92, rss_0.93, ]");
-            System.out.println(" [                  rss_0.94, rss_1.0, rss_2.0 & atom_0.3  ]");
-            System.out.println("The second to last parameters are the URLs of feeds to aggregate.");
-            System.out.println();
+    }
+
+    private static void printEntry(SyndEntry entry) {
+        String summary = "";
+
+        SyndEntry entryImpl = new SyndEntryImpl();
+        entryImpl.setTitle("entry title 1");
+        entryImpl.setUri("http://localhost/feed/item1GUID");
+        entryImpl.setLink("http://localhost/feed/item1");
+
+        summary += "Title: " + entry.getTitle() + "\n";
+
+
+        List<Element> images = entry.getForeignMarkup().stream().filter(element -> element.getNamespacePrefix().equals("media") && element.getName().equals("content")).collect(Collectors.toList());
+
+        if(images.size() > 0){
+            summary += "******** Image: " + images.get(0).getAttributeValue("url") + "\n";
         }
+        //if(entry.getContents().size() > 0) {
+        //    summary += "******** Image: " + entry.getContents().get(0) + "\n";
+        //}
+        summary += "Written by: " + entry.getAuthor() + "\n";
+        summary += "Abstract: " + entry.getDescription().getValue() + "\n";
+        summary += "Published: " + entry.getPublishedDate() + "\n";
+        summary += "Link to article: " + entry.getLink() + "\n";
+        summary += "guID: " + entry.getUri() + "\n";
+
+        System.out.println(summary);
+
     }
 }
--- src/main/java/FeedReader.java
+++ src/main/java/FeedReader.java
@@ -1,9 +1,10 @@
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndEntryImpl;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.io.FeedException;
-import com.sun.syndication.io.SyndFeedInput;
-import com.sun.syndication.io.XmlReader;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndEntryImpl;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 
 import java.io.IOException;
 import java.net.URL;
